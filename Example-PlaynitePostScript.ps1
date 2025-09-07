0aa892341cb3746f66c4b8c5d1eb1ede41819bcd
Eric Barbosa <contact@erictb.com>
2025-09-07 09:56:48 -0400

    fix path on Playnite script for correct location on Android

--- Example-PlaynitePostScript.ps1
+++ Example-PlaynitePostScript.ps1
@@ -3,8 +3,7 @@
 powershell.exe -ExecutionPolicy Bypass -File "C:\Users\erict\OneDrive\Developer\Scripts\Public\Merge Game Saves\MergeGames.ps1" -Path 'C:\Users\erict\OneDrive\Saves\Android\BlackLilyCR\BlueStacks A11','C:\Users\erict\OneDrive\Saves\Android\BlackLilyCR\SwitchAndroid' -Archive "\\192.168.50.49\usb1\Backup\Saves\Automation\BlackLily"
 
 # This runs WHILE the game is running
-powershell.exe -ExecutionPolicy Bypass -File "C:\Users\erict\OneDrive\Developer\Scripts\Public\Merge Game Saves\WinToADB.ps1" -UseLatest -AutoConnect -InitialWait 15 -MaxRetries 5 -RetryDelay 3 -ShowDetails -RemotePath '/sdcard/Android/data/com.crunchyroll.gv.blacklilystale.game/files/Savedata'
-
+powershell.exe -ExecutionPolicy Bypass -File "C:\Users\erict\OneDrive\Developer\Scripts\Public\Merge Game Saves\WinToADB.ps1" -UseLatest -AutoConnect -InitialWait 15 -MaxRetries 5 -RetryDelay 3 -ShowDetails -RemotePath '/sdcard/Android/data/com.crunchyroll.gv.blacklilystale.game/files'
 # This runs AFTER the game closes
 powershell.exe -ExecutionPolicy Bypass -File "C:\Users\erict\OneDrive\Developer\Scripts\scripts\Save File\ADBtoOneDrive.ps1"
 powershell.exe -ExecutionPolicy Bypass -File "C:\Users\erict\OneDrive\Developer\Scripts\Public\Merge Game Saves\MergeGames.ps1" -Path 'C:\Users\erict\OneDrive\Saves\Android\BlackLilyCR\BlueStacks A11','C:\Users\erict\OneDrive\Saves\Android\BlackLilyCR\SwitchAndroid' -Archive "\\192.168.50.49\usb1\Backup\Saves\Automation\BlackLily"
